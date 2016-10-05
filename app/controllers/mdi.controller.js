66ebe124e08c723099fcb90644aeba44f336c30c
ayanth <4ntoine.sf@gmail.com>
2016-10-04 22:51:33 -0400

    Fun with webviews

--- app/controllers/mdi.controller.js
+++ app/controllers/mdi.controller.js
@@ -2,10 +2,16 @@
  * Created by Antoine on 2016-10-01.
  */
 angular.module('Husser').controller('MdiController', ['$scope', 'Socket', 'Tabs',
-    function($scope, Socket, Tabs) {
-        Socket.on('main:connected', function(res) {
-            console.log(res);
-        });
-        Socket.emit('main:connect');
-    }
+	function($scope, Socket, Tabs) {
+		$scope.tab = {
+			src: 'https://www.youtube.com/watch?v=PWgvGjAhvIw'
+		};
+		Socket.on('main:connected', function(res) {
+			console.log(res);
+		});
+		$scope.doExit = function(){
+			console.log('do exit in mdi controller');
+		};
+		Socket.emit('main:connect');
+	}
 ]);
--- app/directives/tabs/browser/browser.js
+++ app/directives/tabs/browser/browser.js
@@ -6,7 +6,28 @@ angular.module('Husser').directive('webBrowser', [
 		return {
 			replace: true,
 			restrict: 'E',
-			templateUrl: '/app/directives/tabs/browser/browser.html'
+			templateUrl: '/app/directives/tabs/browser/browser.html',
+			scope: {
+				tab: '=husTab',
+				exit: '&husExit'
+			},
+			link: function($scope, elem, attrs) {
+				var webview = document.createElement('webview');
+				webview.src = angular.copy($scope.tab.src);
+				webview.style = "width:99vw; height:95vh;display: inline-block";
+				elem[0].appendChild(webview);
+
+				$scope.go = function (where) {
+					webview.go(where);
+				};
+				$scope.navigate = function(where) {
+					webview.src = where;
+				};
+				$scope.exit = function() {
+					console.log('exit in directive');
+				}
+			}
+
 		};
 	}
 ]);
