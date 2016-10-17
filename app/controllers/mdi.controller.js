2b52597d4864c199c07e527c0284637af53246aa
ayanth <4ntoine.sf@gmail.com>
2016-10-16 22:20:03 -0400

    Tab directive

--- app/controllers/mdi.controller.js
+++ app/controllers/mdi.controller.js
@@ -4,15 +4,24 @@
 angular.module('Husser').controller('MdiController', ['$scope', 'Socket', 'Tabs',
 	function($scope, Socket, Tabs) {
 		$scope.tabs = [{
-			src: 'http://youtube.com'
+			src: 'http://youtube.com',
+			isWebBrowser: true
 		}];
 		Socket.on('main:connected', function(res) {
 			console.log(res);
 		});
-		$scope.removeAtZero = function(){
-			console.log('do exit in mdi controller');
-			$scope.tabs.splice(0,1);
+
+		$scope.selectTab = function(index) {
+			for(var i = 0; i < $scope.tabs.length; i++){
+				$scope.tabs[i].visible = false;
+			}
+			$scope.tabs[index].visible = true;
 		};
+
+		// $scope.removeAtZero = function(){
+		// 	console.log('do exit in mdi controller');
+		// 	$scope.tabs.splice(0,1);
+		// };
 		Tabs.added(function(newTab) {
 			console.log($scope.tabs);
 			$scope.tabs.push(newTab);
--- app/directives/hus-tab/hus-tab.js
+++ app/directives/hus-tab/hus-tab.js
@@ -1,20 +1,19 @@
 /**
  * Created by Antoine on 2016-10-02.
  */
-angular.module('Husser').directive('webBrowser', [
+angular.module('Husser').directive('husTab', [
 	function() {
 		return {
 			replace: true,
 			restrict: 'E',
-			templateUrl: '/app/directives/tabs/browser/browser.html',
+			templateUrl: '/app/directives/hus-tab/hus-tab.html',
 			scope: {
-				tab: '=husTab',
-				exit: '&husExit'
+				tab: '=husTab'
 			},
 			link: function($scope, elem, attrs) {
 				var webview = document.createElement('webview');
 				webview.src = angular.copy($scope.tab.src);
-				webview.style = "width:99vw; height:95vh;display: inline-block";
+				webview.style = "width:100%; height:100%;display: inline-block";
 				elem[0].appendChild(webview);
 
 				$scope.go = function (where) {
@@ -23,9 +22,6 @@ angular.module('Husser').directive('webBrowser', [
 				$scope.navigate = function(where) {
 					webview.src = where;
 				};
-				$scope.onExit = function() {
-					console.log('exit in directive');
-				}
 			}
 
 		};
